85291e9eeb6ce81cc7e60bb30acfe4e48127e7ad
Dnahom <kelemntos@gmail.com>
2025-03-31 10:38:57 +0300

    exposed prometheus

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,6 @@ require("dotenv").config({ path: "./src/config/.env" });
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const client = require("prom-client");
-
 const app = express();
 const productsRoute = require("./routes/products.routes");
 const cartRoute = require("./routes/cart.routes");
@@ -27,39 +25,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-// Prometheus metrics setup
-const register = new client.Registry();
-client.collectDefaultMetrics({ register });
+// routes
 
-// Custom HTTP request counter
-const httpRequestCounter = new client.Counter({
-    name: "http_requests_total",
-    help: "Total number of HTTP requests",
-    labelNames: ["method", "route", "status_code"],
-});
-
-app.use((req, res, next) => {
-    res.on("finish", () => {
-        httpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
-    });
-    next();
-});
-
-// Expose /metrics endpoint
-app.get("/metrics", async (req, res) => {
-    res.set("Content-Type", register.contentType);
-    res.end(await register.metrics());
-});
-
-// Routes
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/user", userRoute);
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
     res.send("Shoppify API Ok.");
 });
-
+//
 app.use("*", notFound);
 app.use(errorHandler);
 
